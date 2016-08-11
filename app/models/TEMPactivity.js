ee44e13bc8c26a41da991245813bb5f7f2c4ddda
valmassoi <rvalmassoi@gmail.com>
2016-08-11 11:30:05 -0700

    added transactions to activity list

--- app/models/TEMPactivity.js
+++ app/models/TEMPactivity.js
@@ -19,20 +19,83 @@ module.exports = [//HACK move to backend and in a service
   },
   {
     id: 3,
+    note: "flight to san fran",
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Airlines",
+    pending: false,
+    amount: -95.99,
+    balance: 904.06
+  },
+  {
+    id: 4,
+    note: null,
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Bar/Restaurant",
+    pending: false,
+    amount: -22.29,
+    balance: 881.77
+  },
+  {
+    id: 5,
+    note: null,
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Grocery Store",
+    pending: false,
+    amount: -45.11,
+    balance: 836.66
+  },
+  {
+    id: 6,
+    note: "new clothes",
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Shopping/Retail",
+    pending: false,
+    amount: -85,
+    balance: 751.66
+  },
+  {
+    id: 7,
+    note: "returned clothes, didnt fit",
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Shopping/Retail",
+    pending: false,
+    amount: 85,
+    balance: 836.66
+  },
+  {
+    id: 8,
+    note: null,
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Gas Station",
+    pending: false,
+    amount: -37,
+    balance: 799.66
+  },
+  {
+    id: 9,
+    note: "gotta eat",
+    date: Date.now()-(24*60*60*1000)-21165198,
+    description: "Restaurant",
+    pending: false,
+    amount: -23,
+    balance: 776.66
+  },
+  {
+    id: 10,
     note: "bought some cool thing",
     date: Date.now()-(24*60*60*1000)-21165198,
     description: "Shopping/Retail",
     pending: false,
-    amount: -200.891,
-    balance: 1000.05-200.891
+    amount: -200.89,
+    balance: 575.77
   },
   {
-    id: 4,
-    note: "cool stuff",
+    id: 11,
+    note: "birthday money",
     date: Date.now(),
     description: "Bitcoin top-up",
     pending: true,
     amount: 100,
-    balance: 1000.05-200.891+100//TODO
+    balance: 675.77//TODO
   }
 ]
